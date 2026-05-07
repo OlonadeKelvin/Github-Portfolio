9b7753c1097a62c4f49ce2d9c5210c0c50296f9a
Kelvin Olonade <olonadekelvin@gmail.com>
2026-05-07 16:03:26 +0100

    Updated Profile picture

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { personalInfo, skillGroups, projects } from "./data";
 import "./styles.css";
-import profilePic from './assets/PROFILE.jpg';
+import profilePic from './assets/PROFILE.png';
 
 /* ═══════════════════════════════════════════
    NAVBAR
